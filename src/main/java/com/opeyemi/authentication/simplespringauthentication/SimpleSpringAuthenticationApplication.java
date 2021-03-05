033fe443b75dabadcf99b3dc5ab3962a3bd890da
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-03-05 05:26:25 +0100

    Initial commit

---
+++ src/main/java/com/opeyemi/authentication/simplespringauthentication/SimpleSpringAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.opeyemi.authentication.simplespringauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opeyemi/authentication/simplespringauthentication/SimpleSpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.authentication.simplespringauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
